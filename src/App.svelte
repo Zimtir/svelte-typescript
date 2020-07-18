94c3255ab23dd772342e7750060295c03d495c63
Zimtir <zimtir@mail.ru>
2020-07-18 17:27:02 +0300

    add ts example

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,28 @@
   }
 </style>
 
-<script>
+<script lang="typescript">
+  import { onMount } from 'svelte'
+
   import Spinner from 'svelte-spinner'
+
   import './App.scss'
+
+  let header: string
+
+  const setHeader = (input: string) => {
+    header = input
+  }
+
+  console.log(Spinner)
+
+  onMount(() => setTimeout(() => setHeader('Hello world'), 2000))
 </script>
 
 <div class="container">
-  <h1 class="container-header">Hello world!</h1>
-  <Spinner size="50" speed="750" color="#A82124" thickness="2" gap="40" />
+  {#if header && header.length > 0}
+    <h1 class="container-header">{header}</h1>
+  {:else}
+    <Spinner size="50" speed="750" color="#A82124" thickness="2" gap="40" />
+  {/if}
 </div>
